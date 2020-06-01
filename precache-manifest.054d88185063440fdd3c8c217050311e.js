fe9887fc9d9a88d230e425ecb649818f1b669e3b
bibhu545 <bibhuranjan.545@gmail.com>
2020-06-01 11:31:52 +0530

    Updates

---
+++ precache-manifest.054d88185063440fdd3c8c217050311e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c9cd3036a22d773e27285dda7e3301d",
+    "url": "/ReactJS-Final-Assignment/index.html"
+  },
+  {
+    "revision": "707dcbb98cf2df6ba09c",
+    "url": "/ReactJS-Final-Assignment/static/css/main.e5ad7150.chunk.css"
+  },
+  {
+    "revision": "1497da90350897a2f8b6",
+    "url": "/ReactJS-Final-Assignment/static/js/2.68176bbc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactJS-Final-Assignment/static/js/2.68176bbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "707dcbb98cf2df6ba09c",
+    "url": "/ReactJS-Final-Assignment/static/js/main.d0f5f05e.chunk.js"
+  },
+  {
+    "revision": "59b825217b64005fab5c",
+    "url": "/ReactJS-Final-Assignment/static/js/runtime-main.38a18b25.js"
+  },
+  {
+    "revision": "83b402fab6d07231d889263e867ee4eb",
+    "url": "/ReactJS-Final-Assignment/static/media/bg.83b402fa.jpg"
+  }
+]);
